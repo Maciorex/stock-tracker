8ff89a6e653b222144ab10e28544142abfdc426e
Maciorex <maciej.zabski@gmail.com>
2018-03-15 17:24:02 +0100

    add user and remove user action created

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get "search_stocks", to: "stocks#search"
   get "my_friends", to: "users#my_friends"
   get "search_friend", to: "users#search"
-  get "add_friend", to: "users#add_friend"
+  post "add_friend", to: "users#add_friend"
 end
